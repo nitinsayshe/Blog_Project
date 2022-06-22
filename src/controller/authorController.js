eb3801e37c3baf89f8046e7337f65647d0b5b598
nitinsayshe <nitinsayshe@gmail.com>
2022-06-22 11:53:53 +0530

    js

--- src/controller/authorController.js
+++ src/controller/authorController.js
@@ -3,19 +3,25 @@ const authorModel = require("../models/authorModel")
 
 const authors = async function (req, res) {
     try {
-        let authorsData = req.body
+        let authorsData = req.body;
         console.log(authorsData)
         console.log(req.body)
-        console.log(Object.keys(authorsData).length)
-        
+        console.log(Object.keys(authorsData).length) 
+
         //check if the data in request body is present or not ?
         if (!Object.keys(authorsData).length) {
-            return res.status(400).send({ status: false, msg: "Please Enter the Data in Request Body" })
+            return res.status(400).send({ status: false, msg: "Please Enter the Data in Request Body" });
         }
 
         //check the First & Last Name is present in req.body or not ?
         if (!req.body.fname || !req.body.lname) {
-            return res.status(400).send({ status: false, msg: "Missing Name" })
+            return res.status(400).send({ status: false, msg: "Missing Name" });
+        }
+
+        //check the title is valid or not ?
+        console.log(req.body.title)
+        if(req.body.title in ["Mr", "Mrs", "Miss"]){
+            return res.status(400).send({ status: false, msg: "Title Not Matched" });
         }
 
         //check the email is unique and valid

2b077e17e4861bc0436ca6b6d8b65d8ec51496dc
nitinsayshe <nitinsayshe@gmail.com>
2022-06-22 11:29:03 +0530

    day 2

--- src/controller/authorController.js
+++ src/controller/authorController.js
@@ -4,18 +4,27 @@ const authorModel = require("../models/authorModel")
 const authors = async function (req, res) {
     try {
         let authorsData = req.body
-
-        if (!authorsData || authorsData.length == 0) {
+        console.log(authorsData)
+        console.log(req.body)
+        console.log(Object.keys(authorsData).length)
+        
+        //check if the data in request body is present or not ?
+        if (!Object.keys(authorsData).length) {
             return res.status(400).send({ status: false, msg: "Please Enter the Data in Request Body" })
         }
+
+        //check the First & Last Name is present in req.body or not ?
         if (!req.body.fname || !req.body.lname) {
             return res.status(400).send({ status: false, msg: "Missing Name" })
         }
 
+        //check the email is unique and valid
         let emailFlag = await authorModel.findOne({ email: req.body.email })
         if (emailFlag) {
             return res.status(400).send({ status: false, msg: "E-mail is Already Present in DB" })
         }
+
+        //load the data in database
         let data = await authorModel.create(authorsData)
         return res.status(201).send({ status: true, data: data })
     } catch (error) {
